80bd12cf4f4e653dcd83723c60444ef8e45f3c88
koo <kookh0987@gmail.com>
2017-06-28 00:05:31 +0900

    [add] main menu

--- Contact.py
+++ Contact.py
@@ -21,8 +21,19 @@ def set_contact():
     print(name, phone_number, e_mail, addr)
 
 
+def print_menu():
+    print("1. 연락처 입력")
+    print("2. 연락처 출력")
+    print("3. 연락처 삭제")
+    print("4. 종료")
+    menu=input("메뉴선택: ")
+    return int(menu)
+
 def run():
-    set_contact()
+    while 1:
+        menu=print_menu()
+        if menu==4:
+            break
 
 if __name__=="__main__":
     run()
